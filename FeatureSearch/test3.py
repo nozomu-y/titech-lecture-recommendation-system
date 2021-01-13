8faf7ca75d2c0fde344ed3b00a471b8e0f9e2621
Masada Tatsuya <masada.t.aa@m.titech.ac.jp>
2021-01-13 22:20:06 +0900

    (fix kouginator) fix test3

--- FeatureSearch/test3.py
+++ FeatureSearch/test3.py
@@ -1,9 +1,10 @@
 import feature_search
 
 subject_codes = []
-
-fs = feature_search()
-fs.make_init_index_list()
+'''
+科目コードを作成する動作
+'''
+fs = feature_search(subject_codes)
 fs.get_features()
 index_list = fs.get_index_list()
 print("----------該当講義一覧----------")
