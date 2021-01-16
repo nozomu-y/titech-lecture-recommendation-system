9374d62ee79692f73c8e24d26bfe985aabdf0e91
nozoyama39 <nozoyama39@gmail.com>
2021-01-16 12:02:26 +0900

    Update: lecture_codes.json
    include relative lectures

--- DataCollection/concat_json.py
+++ DataCollection/concat_json.py
@@ -3,19 +3,28 @@ import json
 
 json_paths = os.listdir(path='./output/')
 syllabus = []
-lectures = {}
 
 for json_path in json_paths:
     if '.json' in json_path:
         f = open('./output/' + json_path, 'r')
         data = json.load(f)
         syllabus.extend(data)
-        for lecture in syllabus:
-            lectures[lecture['科目コード']] = lecture['講義名']
         f.close()
 
 f = open('syllabus.json', 'w')
 json.dump(syllabus, f, ensure_ascii=False, indent=4)
+f.close()
+
+
+lectures = {}
+
+for lecture in syllabus:
+    lectures[lecture['科目コード']] = lecture['講義名']
+    if '関連する科目' in lecture.keys():
+        lectures[lecture['科目コード']]['関連する科目'] = []
+        for related_lecture in lecture['関連する科目']:
+            if '科目コード' in related_lecture.keys():
+                lectures[lecture['科目コード']]['関連する科目'].append(related_lecture['科目コード'])
 f.close()
 
 f = open('lecture_codes.json', 'w')
