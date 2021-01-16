bbc811f9010cf5b23cd6a50a5ff02d8db82cd6dd
nozoyama39 <nozoyama39@gmail.com>
2021-01-16 17:28:02 +0900

    Add: vectorizer.pkl

--- Clustering/makepkl.py
+++ Clustering/makepkl.py
@@ -1,7 +1,18 @@
+import getscore
+from getname import GetNameJ
+import numpy
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+import matplotlib.pyplot as plt
+import hierarchy_cluster
+import getname
+from sklearn import model_selection
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+from sklearn import mixture, cluster
 import json
-import pandas as pd 
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-docs=[]
+docs = []
 path_open = open('path_clustering.json', 'r')
 paths = json.load(path_open)
 for path in paths.values():
@@ -10,38 +21,28 @@ for path in paths.values():
     docs.append(words)
 vectorizer = TfidfVectorizer(use_idf=True, token_pattern=u'(?u)\\b\\w+\\b')
 vecs = vectorizer.fit_transform(docs)
-from sklearn import mixture, cluster
-import pandas as pd
-from sklearn.model_selection import GridSearchCV
-from sklearn import model_selection
-import getname
-import hierarchy_cluster
-import matplotlib.pyplot as plt
-from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 print('start')
-df = pd.DataFrame(vecs.toarray(),columns=vectorizer.get_feature_names())
-df["subject"]=list(paths.keys())
+df = pd.DataFrame(vecs.toarray(), columns=vectorizer.get_feature_names())
+df["subject"] = list(paths.keys())
 df = df.set_index("subject")
-gmm=hierarchy_cluster.hierarchy_cluster(df)
+gmm = hierarchy_cluster.hierarchy_cluster(df)
 
 print('end')
 lis = []
 plt.figure(num=None, figsize=(16, 9), dpi=200, facecolor='w', edgecolor='k')
-dendrogram(gmm,labels=df.index)
+dendrogram(gmm, labels=df.index)
 plt.show()
-import numpy
-clusters = fcluster(gmm,1.2*numpy.average(gmm[:,2]), criterion='distance')
-from getname import GetNameJ
+clusters = fcluster(gmm, 1.2 * numpy.average(gmm[:, 2]), criterion='distance')
 for doc, cls in zip(paths.keys(), clusters):
     lis.append((cls, GetNameJ(doc)))
 lis.sort()
 
 print(max(clusters))
 print(type(clusters))
-import getscore
 
-#pd.to_pickle(,"pkl/train_codes.pkl")
+# pd.to_pickle(,"pkl/train_codes.pkl")
 #pd.to_pickle(train_docs, "pkl/train_docs.pkl")
-pd.to_pickle(vecs,"pkl/vec.pkl")
-pd.to_pickle(clusters,"pkl/clusters.pkl")
-pd.to_pickle(gmm,"pkl/gmm.pkl")
+pd.to_pickle(vecs, "pkl/vec.pkl")
+pd.to_pickle(clusters, "pkl/clusters.pkl")
+pd.to_pickle(gmm, "pkl/gmm.pkl")
+pd.to_pickle(vectorizer, "pkl/vectorizer.pkl")
