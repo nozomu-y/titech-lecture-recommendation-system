8c2f7adf3ee91da9170b14c622f3923d439bd3de
nozoyama39 <nozoyama39@gmail.com>
2021-01-16 11:47:59 +0900

    Update: reduce computational cost

--- Clustering/getname.py
+++ Clustering/getname.py
@@ -1,82 +1,50 @@
 
-#科目コードから講義名(日本語)をえる
+# 科目コードから講義名(日本語)をえる
 def GetNameJ(code):
     import json
-    syllabus_paths = '../DataCollection/syllabus.json'
-    '''
-    syllabus_paths = [
-        '../DataCollection/output/csc_2020.json',
-        '../DataCollection/output/csc_2019.json',
-        '../DataCollection/output/csc_2018.json',
-        '../DataCollection/output/csc_2017.json',
-        '../DataCollection/output/csc_2016.json'
-    ]
-    '''
-    str=''
-    with open(syllabus_paths) as f:
-        lectures = json.load(f)    
-    for lecture in lectures:
-        str=lecture['科目コード']
-        if(code==str):
-            return lecture['講義名']['日本語']
-    return 'not_exit'
+    lecture_codes = '../DataCollection/lecture_codes.json'
+    with open(lecture_codes) as f:
+        lectures = json.load(f)
+    if code in lectures.keys():
+        return lectures[code]['日本語']
+    return None
 
-#科目コードから講義名(英語)を得る
+# 科目コードから講義名(英語)を得る
+
+
 def GetNameE(code):
     import json
-    #syllabus_path = '../DataCollection/output.json'
-    syllabus_paths = '../DataCollection/syllabus.json'
-    '''
-    syllabus_paths = [
-        '../DataCollection/output/csc_2020.json',
-        '../DataCollection/output/csc_2019.json',
-        '../DataCollection/output/csc_2018.json',
-        '../DataCollection/output/csc_2017.json',
-        '../DataCollection/output/csc_2016.json'
-    ]
-    '''
-    str=''
-    for path in syllabus_paths:
-        with open(path) as f:
-            lectures = json.load(f)
-    
-        for lecture in lectures:
-            str=lecture['科目コード']
-            if(code==str):
-                return lecture['講義名']['英語']
-    return 'not_exit'
+    lecture_codes = '../DataCollection/lecture_codes.json'
+    with open(lecture_codes) as f:
+        lectures = json.load(f)
+    if code in lectures.keys():
+        return lectures[code]['英語']
+    return None
 
-#科目コードから、関連する科目の科目コードを得る
+# 科目コードから、関連する科目の科目コードを得る
+
+
 def GetRelaSub(code):
     import json
     syllabus_paths = '../DataCollection/syllabus.json'
-    #syllabus_path = '../DataCollection/output.json'
-    '''
-    syllabus_paths = [
-        '../DataCollection/output/csc_2020.json',
-        '../DataCollection/output/csc_2019.json',
-        '../DataCollection/output/csc_2018.json',
-        '../DataCollection/output/csc_2017.json',
-        '../DataCollection/output/csc_2016.json'
-    ]
-    '''
-    str=''
+    str = ''
     for path in syllabus_paths:
         with open(path) as f:
             lectures = json.load(f)
         for lecture in lectures:
-            str=lecture['科目コード']
-            if(code==str):
+            str = lecture['科目コード']
+            if(code == str):
                 if '関連する科目' in lecture.keys():
-                    relaSubs=lecture['関連する科目']
-                    lis=[]
+                    relaSubs = lecture['関連する科目']
+                    lis = []
                     for sub in relaSubs:
                         if '科目コード' in sub.keys():
                             lis.append(sub['科目コード'])
-                        
+
                     return lis
     return []
-   
+
+
 """
 if __name__ == '__main__':
     GetNameJ(code)
