5026de8caa0ee3442db7084685e9f3b42f13ebc3
nozoyama39 <nozoyama39@gmail.com>
2021-01-16 11:42:39 +0900

    Create: lecture_codes.json

--- DataCollection/concat_json.py
+++ DataCollection/concat_json.py
@@ -2,16 +2,22 @@ import os
 import json
 
 json_paths = os.listdir(path='./output/')
-lectures = []
+syllabus = []
+lectures = {}
 
 for json_path in json_paths:
     if '.json' in json_path:
-        print(json_path)
         f = open('./output/' + json_path, 'r')
         data = json.load(f)
-        lectures.extend(data)
+        syllabus.extend(data)
+        for lecture in syllabus:
+            lectures[lecture['科目コード']] = lecture['講義名']
         f.close()
 
 f = open('syllabus.json', 'w')
+json.dump(syllabus, f, ensure_ascii=False, indent=4)
+f.close()
+
+f = open('lecture_codes.json', 'w')
 json.dump(lectures, f, ensure_ascii=False, indent=4)
 f.close()
